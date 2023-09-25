8ee493a36127e8e55dacb20d6c0377a9c2802682
Craig Berry <csberry123@outlook.com>
2023-09-25 08:53:41 +0100

    Added buffer viewport to fix error spam, set MaxFPS to 60, fixing issue where shader would simulate too fast.

--- main.gd
+++ main.gd
@@ -7,9 +7,6 @@ func _ready():
 	var water = $Water
 	var sim_tex = $Simulation.get_texture()
 	var col_tex = $Collision.get_texture()
-	
-	cr.material.set_shader_parameter('sim_tex', sim_tex)
-	cr.material.set_shader_parameter('col_tex', col_tex)
 
 	water.mesh.surface_get_material(0).set_shader_parameter('simulation', sim_tex)
 	water.mesh.surface_get_material(0).set_shader_parameter('simulation2', sim_tex)
